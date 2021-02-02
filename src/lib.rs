2a22e5f196b03658df4dc334e33446fe64e9471e
Kasper <kasperkh.kh@gmail.com>
2021-02-02 04:42:37 +0100

    Fix `fmt::Display` for errors

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,7 @@ impl Error {
 
 impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "Oh no, something bad went down")
+    write!(f, "{}", self.message())
   }
 }
 
